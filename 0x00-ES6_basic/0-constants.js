b45f247c1ac2e04994d09ef9d6080051e7ffd643
RamzyAR7 <rameya683@gmail.com>
2024-07-22 23:57:09 +0300

    add v0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,6 @@
+export const taskFirst = () => 'I prefer const when I can.'
+export const getLast = () => ' is okay'
+export let taskNext = () => {
+    var combination = 'But sometimes let';
+    combination += getLast() 
+}
