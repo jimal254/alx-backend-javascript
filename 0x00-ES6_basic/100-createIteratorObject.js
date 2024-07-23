8cb1cee6db2fe299d3ed50c7d7cd928dc6f4db7a
RamzyAR7 <rameya683@gmail.com>
2024-07-23 17:50:35 +0300

    add v100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,13 @@
 export default function createIteratorObject(report) {
-  return (function* generator() {
-    for (const department of Object.values(report.allEmployees)) {
+  // Generator function to yield each employee
+  function* employeeGenerator() {
+    for (const department of Object.values(report)) {
       for (const employee of department) {
         yield employee;
       }
     }
-  }());
+  }
+
+  // Return the generator object
+  return employeeGenerator();
 }
