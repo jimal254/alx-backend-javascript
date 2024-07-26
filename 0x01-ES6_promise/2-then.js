786753703e1b737450ca5156ff664ef2c145e401
RamzyAR7 <rameya683@gmail.com>
2024-07-26 22:46:07 +0300

    add v2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+
+  const res = promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => Error())
+}
