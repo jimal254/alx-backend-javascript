890ab7dc51ef27c7ad0a523a77311426fb8b2f76
RamzyAR7 <rameya683@gmail.com>
2024-07-27 00:11:08 +0300

    add v9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const res = mathFunction();
+    queue.push(res);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
