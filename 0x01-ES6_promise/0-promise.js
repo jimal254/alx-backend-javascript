64b5b65dcba38b18916a3cc1ce07a17e69180d6b
RamzyAR7 <rameya683@gmail.com>
2024-07-26 22:14:27 +0300

    add v0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,5 @@
 export default function getResponseFromAPI() {
-  const promise = new Promise((resolve) => {
+  return new Promise((resolve) => {
     resolve('hi, how are you');
   });
-
-  return promise;
 }
