eec91e8ed85a9648c04cba24d39d12f68180b75f
RamzyAR7 <rameya683@gmail.com>
2024-07-31 02:02:14 +0300

    add v4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,17 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeInfo = newGrades.find(grade => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: gradeInfo ? gradeInfo.grade : 'N/A'
-      };
-    });
+  const defaultGrade = { grade: 'N/A' };
+
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
